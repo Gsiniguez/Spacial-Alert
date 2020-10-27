588e8df223619582f7c591340cbc62a759b0f776
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2020-10-27 11:17:16 -0300

    Camera Funcion SeguirNave()

---
+++ Assets/Scripts/Camara/Controller_Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Camera : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject nave;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SeguirNave();
+    }
+
+
+    void SeguirNave()
+    {
+        transform.position = new Vector3(nave.transform.position.x, nave.transform.position.y, -10);
+    }
+}
