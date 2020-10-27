7c9518da919bcf6cb15541535cc607b24cb11d7e
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2020-10-27 08:42:03 -0300

    Funcion Impulsar

--- Assets/Scripts/Nave/Controller_Nave.cs
+++ Assets/Scripts/Nave/Controller_Nave.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Controller_Nave : Events_Nave
 {
+
+    [SerializeField]
+    float speed;
+
+    Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            Impulsar();
+        }
+    }
+
+    void Impulsar()
+    {
+        float mvm = 1;
+        float move = mvm * speed;
+        rigidbody.AddForce(new Vector2(rigidbody.velocity.x, move));
+    }
+
+    void Rotar()
+    {
+
     }
 }
