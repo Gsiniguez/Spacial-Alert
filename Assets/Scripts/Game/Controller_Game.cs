6fed592cdeba5ad755e0eb2d732b2fc24e025be4
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2020-11-13 21:20:31 -0300

    Controlle_Gamer => PuntosPorMilisegundo()

---
+++ Assets/Scripts/Game/Controller_Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Game : Events_Game
+{
+
+    [SerializeField]
+    float puntos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void PuntosPorMilisegundo()
+    {
+        puntos += Time.deltaTime;
+    }
+}
